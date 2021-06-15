2ed763c8d319ba75946fb4106c72ffd09527be5d
Arark <knkr5456@gmail.com>
2021-06-15 23:16:54 +0900

    feat(member): add Link for detailed page

--- pages/member/index.tsx
+++ pages/member/index.tsx
@@ -1,6 +1,7 @@
 import { InferGetStaticPropsType } from "next";
 import { getMembers } from "../../lib/resource";
 import Image from "next/image";
+import Link from "next/link";
 import { roles, roles_en } from "../../lib/roles";
 
 export default function Member({
@@ -15,28 +16,31 @@ export default function Member({
           return (
             <div>
               <h2>
-                {role}  {roles[role]}
+                {role} {roles[role]}
               </h2>
               {people.map((person) => {
                 return (
                   <div>
-                    <div
-                      style={{
-                        position: "relative",
-                        width: "120px",
-                        height: "120px",
-                      }}
-                    >
-                      <Image
-                        src={person.photoURL}
-                        layout="fill"
-                        objectFit="contain"
-                        objectPosition=""
-                      />
-                    </div>
-                    <p>
-                      {person.name?.ja}, {person.name?.en}
-                    </p>
+                    <Link href={`/member/${person.slug}`}>
+                      <a>
+                        <div
+                          style={{
+                            position: "relative",
+                            width: "120px",
+                            height: "120px",
+                          }}
+                        >
+                          <Image
+                            src={person.photoURL}
+                            layout="fill"
+                            objectFit="contain"
+                          />
+                        </div>
+                        <p>
+                          {person.name?.ja}, {person.name?.en}
+                        </p>
+                      </a>
+                    </Link>
                     <p>
                       {person.position?.ja}, {person.position?.en}
                     </p>
