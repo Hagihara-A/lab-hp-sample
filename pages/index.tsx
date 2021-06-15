b65b3b888674d9bde34b22a095f1e11e0d8806f2
Arark <knkr5456@gmail.com>
2021-06-16 00:59:41 +0900

    styling(index): add bg-image, logo and margins

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,14 @@
-import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import Link from "next/link";
-
+import s from "../styles/index.module.scss";
 export default function Home() {
   return (
-    <>
-      
-      <main>
-        <h1>aTeal</h1>
-        <h2>Analog Technology and Application Lab.</h2>
-        <h3>Ito Laboratory, Nano Sensing Unit, Tokyo Tech</h3>
-      </main>
-    </>
+    <main className={s.main}>
+      <div className={s.logo}>
+        <Image src="/logo-white.svg" layout="fill" objectFit="contain" />
+      </div>
+
+      <h2>Analog Technology and Application Lab.</h2>
+      <h3>Ito Laboratory, Nano Sensing Unit, Tokyo Tech</h3>
+    </main>
   );
 }
