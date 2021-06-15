5e6320f863574b3789572e30e4c5a57eeb87965e
Arark <knkr5456@gmail.com>
2021-06-15 22:51:55 +0900

    feat(member): impl index page

---
+++ pages/member/index.tsx
@@ -0,0 +1,58 @@
+import { InferGetStaticPropsType } from "next";
+import { getMembers } from "../../lib/resource";
+import Image from "next/image";
+import { roles, roles_en } from "../../lib/roles";
+
+export default function Member({
+  members,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <main>
+      <h1>メンバー一覧</h1>
+      <div>
+        {roles_en.map((role) => {
+          const people = members.filter((member) => member.role == role);
+          return (
+            <div>
+              <h2>
+                {role}  {roles[role]}
+              </h2>
+              {people.map((person) => {
+                return (
+                  <div>
+                    <div
+                      style={{
+                        position: "relative",
+                        width: "120px",
+                        height: "120px",
+                      }}
+                    >
+                      <Image
+                        src={person.photoURL}
+                        layout="fill"
+                        objectFit="contain"
+                        objectPosition=""
+                      />
+                    </div>
+                    <p>
+                      {person.name?.ja}, {person.name?.en}
+                    </p>
+                    <p>
+                      {person.position?.ja}, {person.position?.en}
+                    </p>
+                    <p>{person.email?.replace("@", "[at]")}</p>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </main>
+  );
+}
+export const getStaticProps = async () => {
+  const members = await getMembers();
+
+  return { props: { members } };
+};
