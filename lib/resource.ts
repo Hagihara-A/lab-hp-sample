593296744db6cf341e0fcfae03dcbc12c4ce94cf
Arark <knkr5456@gmail.com>
2021-06-15 22:50:05 +0900

    feat(resource): impl getMembers

--- lib/resource.ts
+++ lib/resource.ts
@@ -2,9 +2,11 @@ import * as path from "path";
 import * as fs from "fs/promises";
 import matter from "gray-matter";
 import marked from "marked";
+import { Roles, roles_en } from "./roles";
 
 const contentsDir = path.join(process.cwd(), "/contents");
 const newsDir = path.join(contentsDir, "news");
+const memberDir = path.join(contentsDir, "member");
 export async function getAboutHTML() {
   const fpath = path.join(contentsDir, "about.md");
   const md = await fs.readFile(fpath);
@@ -40,4 +42,45 @@ export async function getLocationHTML() {
   const content = await fs.readFile(filePath, "utf-8");
   const html = marked(content);
   return html;
+}
+
+interface MemberProfile {
+  name: { ja: string; en?: string };
+  photoURL: `/portrait/${string}`;
+  email?: string;
+  position?: { ja?: string; en?: string };
+  role: Roles;
+  detail: string;
+}
+export async function getMembers(): Promise<MemberProfile[]> {
+  const memberProfiles = await Promise.all(
+    roles_en.map(async (role) => {
+      const roleDir = path.join(memberDir, role);
+      const persons = await fs.readdir(roleDir);
+
+      const profiles = await Promise.all(
+        persons.map(async (person): Promise<MemberProfile> => {
+          const personFile = path.join(roleDir, person);
+          const personContent = await fs.readFile(personFile);
+          const { data, content } = matter(personContent);
+          const detail = marked(content);
+          return {
+            name: { ja: data.name_ja, en: data.name_en },
+            position: {
+              ja: data.position_ja,
+              en: data.position_en,
+            },
+            photoURL: data.photoURL
+              ? `/portrait/${data.photoURL}`
+              : `/portrait/nopic.jpg`,
+            email: data.email,
+            detail,
+            role: role as Roles,
+          };
+        })
+      );
+      return profiles;
+    })
+  );
+  return memberProfiles.flat();
 }
