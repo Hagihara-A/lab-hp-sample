bab81eef788f12309d3967bf21a08866643afab8
Arark <knkr5456@gmail.com>
2021-06-16 00:59:08 +0900

    styling(_app): add logo and fix margin etc..

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import "../styles/globals.css";
+import hd from "../styles/header.module.scss";
+import ft from "../styles/footer.module.scss";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,13 +12,22 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <title>aTeal - 伊藤研究室 | 東京工業大学 科学技術創成研究院</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <header>
-        <Link href="/">
-          <a>aTeal</a>
-        </Link>
+      <header className={hd.header}>
+        <div>
+          <Link href="/">
+            <a>
+              <Image
+                src="/LogoTypo.svg"
+                layout="fill"
+                objectFit="contain"
+                alt="ateal"
+              />
+            </a>
+          </Link>
+        </div>
         <nav>
           <Link href="/about">
-            <a>About aTeal</a>
+            <a>About</a>
           </Link>
           <Link href="/news">
             <a>News</a>
@@ -36,7 +48,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       </header>
       <Component {...pageProps} />
 
-      <footer>©2021 aTeal 伊藤研究室 All rights reserved.</footer>
+      <footer className={ft.footer}>
+        ©2021 aTeal 伊藤研究室 All rights reserved.
+      </footer>
     </>
   );
 }
