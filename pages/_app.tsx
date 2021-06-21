7bd3c760efcef1a89c0584acfc546a35b646f1df
Arark <knkr5456@gmail.com>
2021-06-21 20:25:51 +0900

    fix(header): close menu on click link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { useState, useCallback } from "react";
 import "../styles/globals.scss";
 import hd from "../styles/header.module.scss";
 import ft from "../styles/footer.module.scss";
@@ -5,7 +6,12 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { slide as Menu } from "react-burger-menu";
+
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const onMenuStateChange = useCallback((s) => setIsMenuOpen(s.isOpen), []);
+  const closeMenu = useCallback(() => setIsMenuOpen(false), []);
   return (
     <>
       <Head>
@@ -25,26 +31,35 @@ export default function MyApp({ Component, pageProps }: AppProps) {
             </a>
           </Link>
         </div>
-        <nav className={hd.nav}>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-          <Link href="/news">
-            <a>News</a>
-          </Link>
-          <Link href="/research">
-            <a>Research</a>
-          </Link>
-          <Link href="/members">
-            <a>Members</a>
-          </Link>
-          <Link href="/publications">
-            <a>Publications</a>
-          </Link>
-          <Link href="/location">
-            <a>Location</a>
-          </Link>
-        </nav>
+        <Menu
+          right
+          isOpen={isMenuOpen}
+          onStateChange={onMenuStateChange}
+          noTransition
+        >
+          <MenuLink href="/about" displayTxt="About" closeMenu={closeMenu} />
+          <MenuLink href="/news" displayTxt="News" closeMenu={closeMenu} />
+          <MenuLink
+            href="/members"
+            displayTxt="Members"
+            closeMenu={closeMenu}
+          />
+          <MenuLink
+            href="/research"
+            displayTxt="Research"
+            closeMenu={closeMenu}
+          />
+          <MenuLink
+            href="/publications"
+            displayTxt="Publications"
+            closeMenu={closeMenu}
+          />
+          <MenuLink
+            href="/location"
+            displayTxt="Location"
+            closeMenu={closeMenu}
+          />
+        </Menu>
       </header>
       <Component {...pageProps} />
 
@@ -52,5 +67,21 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         ©2021 aTeal 伊藤研究室 All rights reserved.
       </footer>
     </>
+  );
+}
+
+function MenuLink({
+  href,
+  displayTxt,
+  closeMenu,
+}: {
+  href: `/${string}`;
+  displayTxt: string;
+  closeMenu: () => void;
+}) {
+  return (
+    <Link href={href}>
+      <a onClick={closeMenu}>{displayTxt}</a>
+    </Link>
   );
 }
